39ea04a518a3303192d5898ee90ec46dc26a4cd2
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2021-06-27 16:42:57 +0900

    Add test for `path` argument

--- main_test.go
+++ main_test.go
@@ -6,6 +6,7 @@ import (
 	"io/ioutil"
 	"os"
 	"os/exec"
+	"path"
 	"strings"
 	"testing"
 	"time"
@@ -66,5 +67,39 @@ func TestIgnore(t *testing.T) {
 	want := "run command"
 	if strings.Contains(got, want) {
 		t.Fatalf("expected \n%s\n\nnot included, but got \n\n%s\n", want, got)
+	}
+}
+
+func TestPath(t *testing.T) {
+	tempDir, err := ioutil.TempDir("", "kurogotest")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer os.RemoveAll(tempDir)
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	cmd := exec.CommandContext(ctx, "./kurogo", "-f", "testdata/no_ignore.toml", "-p", tempDir)
+
+	stdout, err := cmd.StdoutPipe()
+	if err != nil {
+		t.Fatalf("stdout getting failed: %v\n", err)
+	}
+
+	if err := cmd.Start(); err != nil {
+		t.Fatalf("command start failed: %v\n", err)
+	}
+
+	time.Sleep(2 * time.Second)
+	ioutil.WriteFile(path.Join(tempDir, "main.go"), []byte("Hello"), 0644)
+
+	buf := new(bytes.Buffer)
+	buf.ReadFrom(stdout)
+	got := buf.String()
+
+	want := "run command"
+	if !strings.Contains(got, want) {
+		t.Fatalf("expected \n%s\n\nbut got \n\n%s\n", want, got)
 	}
 }
